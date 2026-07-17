71906d4fbee9096d8b45505f320d66ec228284d0
alago_0 <alago_0@proton.me>
2026-07-17 18:23:50 +0300

    wip fetch-decode-execute-loop

--- chip8/src/chip8.h
+++ chip8/src/chip8.h
@@ -21,6 +21,7 @@ Chip8 chip8_init();
 void chip8_destroy(Chip8* chip8);
 void chip8_load_rom(Chip8* chip8,
                     const char* path);
+void chip8_run(Chip8* chip8);
 
 
 #endif
--- meta-workspace/completed/core-init.md
+++ meta-workspace/completed/core-init.md
@@ -1,3 +1,4 @@
 Create init for core (each number should have function):
   1. Set all variables to 0 - DONE
   2. Load fonts - DONE
+  3. Load rom - DONE
